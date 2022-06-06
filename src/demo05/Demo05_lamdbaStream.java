f2abbd1f1f221d9f5d9e61293eeabae435df86e5
zhanghy <zhanghongyu@yonyou.com>
2022-06-07 00:06:01 +0800

    20220606-Zhanghy-Java8-listToSet

---
+++ src/demo05/Demo05_lamdbaStream.java
@@ -0,0 +1,37 @@
+package demo05;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class Demo05_lamdbaStream {
+    public static void main(String[] args) {
+        UserVO userVO1 = new UserVO("ZHY", 20);
+        UserVO userVO2 = new UserVO("ZHY", 20);
+
+        // 在默认的情况下 equals 方法比较两个对象的内存地址是否相等
+        System.out.println(userVO1.equals(userVO2));
+        System.out.println("1".equals("1"));
+
+        HashMap<UserVO, String> map = new HashMap<>();
+        map.put(userVO1, "abc");
+        map.put(userVO2, "def");
+        map.forEach((k,v)->{
+            if (map.containsKey(k)){
+                System.out.println("key: " + k + " --- " + "value: " + v);
+            } else {
+                map.put(k, v);
+            }
+        });
+
+        ArrayList<Object> arrayList = new ArrayList<>();
+        arrayList.add(userVO1);
+        arrayList.add(userVO2);
+
+        Set<Object> objectSet = arrayList.stream().collect(Collectors.toSet());
+        objectSet.forEach((s)-> {
+            System.out.println(s);
+        } );
+    }
+}
---
+++ src/demo05/UserVO.java
@@ -0,0 +1,49 @@
+package demo05;
+
+public class UserVO {
+     private String name;
+     private Integer age;
+
+     public UserVO(String name, Integer age) {
+          this.name = name;
+          this.age = age;
+     }
+
+     public String getName() {
+          return name;
+     }
+
+     public void setName(String name) {
+          this.name = name;
+     }
+
+     public Integer getAge() {
+          return age;
+     }
+
+     public void setAge(Integer age) {
+          this.age = age;
+     }
+
+     @Override
+     public int hashCode() {
+          return name.hashCode();
+     }
+
+     @Override
+     public boolean equals(Object obj) {
+          if (obj instanceof UserVO) {
+               return name.equals(((UserVO) obj).name) && age.equals(((UserVO)obj).age);
+          } else {
+               return false;
+          }
+     }
+
+     @Override
+     public String toString() {
+          return "{UserVO" +
+                  "Name:" + name + '\'' +
+                  ", age= " + age +
+                  '}';
+     }
+}
